48bf16c33ee712f1937a0bfe9f8f4acca6308290
izbk <izbk@163.com>
2020-03-24 23:01:17 +0800

    add votaile demo

--- src/main/java/com/atguigu/interview/juc/VolatileDemo.java
+++ src/main/java/com/atguigu/interview/juc/VolatileDemo.java
@@ -1,16 +1,51 @@
 package com.atguigu.interview.juc;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class MyData{
     // 可见性验证，可注释后体验效果
     volatile int number = 0;
+    AtomicInteger atomic = new AtomicInteger(0);
     public void add(){
         this.number = 10;
+    }
+    public void plus(){
+        this.number++;
+    }
+    public void atomicPlus(){
+        atomic.getAndIncrement();
     }
 }
 public class VolatileDemo {
     public static void main(String[] args) {
+        noAtomicVolatile();
+    }
+
+    /**
+     * 不保证原子性验证
+     */
+    private static void noAtomicVolatile() {
+        MyData myData = new MyData();
+        for (int i = 0; i < 20 ; i++) {
+            new Thread(()->{
+                for (int j = 0; j < 1000; j++) {
+                    myData.plus();
+                    myData.atomicPlus();
+                }
+            },String.valueOf(i)).start();
+        }
+        while(Thread.activeCount() >2){
+            Thread.yield();
+        }
+        System.out.println(Thread.currentThread().getName() + " finally number is :"+myData.number);
+        System.out.println(Thread.currentThread().getName() + " finally atomic is :"+myData.atomic);
+    }
+
+    /**
+     * 可见性验证
+     */
+    private static void seeOkByVolatile() {
         MyData myData = new MyData();
         new Thread(()->{
             System.out.println(Thread.currentThread().getName());
